1e1f96c895eec2e651ec03dfe9b53c3ac1acbed7
lgope <ilakshmangope@gmail.com>
2023-12-03 14:33:18 +0600

    typescript interface 🚀

---
+++ interface.ts
@@ -0,0 +1,27 @@
+/**
+ * INTERFACES
+ */
+
+// Be aware no equal sign
+interface IUser {
+  username: string;
+  email: string;
+  age: number;
+}
+
+interface IEmployee extends IUser {
+  employeeId: number;
+}
+
+const emp: IEmployee = {
+  username: "tom",
+  email: "tom@gmail.com",
+  age: 43,
+  employeeId: 1,
+};
+
+const client: IUser = {
+  username: "tom",
+  email: "tom@gmail.com",
+  age: 43,
+};
