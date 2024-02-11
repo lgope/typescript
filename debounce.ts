5a8b2abc9badf0b3409a8475de9560eaa13e42e8
Lakshman Gope <ilakshmangope@gmail.com>
2024-02-11 17:26:15 +0600

    Update debounce.ts

--- debounce.ts
+++ debounce.ts
@@ -11,3 +11,13 @@ function debounce<T extends Function>(cb: T, wait = 20) {
 const func = debounce((a: string, b: number, c?: number) => console.log(a.length + b + c || 0));
 func("Hello", 1, 1);
 func("Hellow World", 1);
+
+
+// Another Approach 
+const debounce = (fn: Function, ms = 300) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
